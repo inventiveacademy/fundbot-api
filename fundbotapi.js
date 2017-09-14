a3b3ef8c5c705da014701b9b81f95c2b87809861
Scottie Schneider <scott@laclave.co>
2017-09-14 16:34:29 -0500

    Squashed commit of the following:
    commit 1ace0c51a03c645fbe771ffdbe58d5652ae5b31a
    Author: Scottie Schneider <scott@laclave.co>
    Date:   Thu Sep 14 16:33:59 2017 -0500
    
        3
    
    commit da262ef8e169c91dceed6f62bd792d7c3b574e92
    Author: Scottie Schneider <scott@laclave.co>
    Date:   Thu Sep 14 16:33:45 2017 -0500
    
        2
    
    commit 4478e9e659156bc47abc91059485d6bb80041768
    Author: Scottie Schneider <scott@laclave.co>
    Date:   Thu Sep 14 16:33:29 2017 -0500
    
        1

--- fundbotapi.js
+++ fundbotapi.js
@@ -1,4 +1,7 @@
 // mongoose 4.3.x
+// squash 1
+// squash 2
+// squash 3
 var mongoose = require('mongoose')
 var restify = require('restify')
 var session = require('express-session')
@@ -78,7 +81,7 @@ get '/getdeletedapplications'            = get all deleted applications
 
 post '/applications'                     = create a new application
 post '/login?user=xxx&pwd=yyy'           = login and set lastlogindate
-post '/createlogin?user=xxx&pwd=yyy'     = create login 
+post '/createlogin?user=xxx&pwd=yyy'     = create login
     note: you can also add isstudent, isadmin, and isapplicant as parameters
     defaults are isapplicant=true, isadmin=false, isstudent=false
 
@@ -98,7 +101,7 @@ let db = mongoose.connection;
 //mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
 
-// Wait for the database connection to establish, then start the app.   
+// Wait for the database connection to establish, then start the app.
 db.once('open', function() {
     var port = 3008
     server.listen(port, function() {
@@ -296,7 +299,7 @@ function postApplication(req, res, next) {
 function login(req, res, next) {
 	bcrypt.hash(req.query.pwd,SALT_ROUNDS,function(err, hash){
 		console.log("user: " + req.query.user + " pwd: " + req.query.pwd + " hash: "+hash)
-		
+
 	    Login.findOne({ "user": req.query.user}, function(err, user) {
 	        if (err) {
 	            console.log(err)
@@ -307,7 +310,7 @@ function login(req, res, next) {
 	            } else {
 	            	bcrypt.compare(req.query.pwd, hash, function(err, tf) {
 					    if (tf) {
-						
+
 			                var hour = 3600000
 			                req.session.cookie.expires = new Date(Date.now() + hour)
 			                req.session.user = user.user
@@ -315,7 +318,7 @@ function login(req, res, next) {
 			                user.lastlogin = new Date()
 			                user.isloggedin = true
 				            user.save()
-			                
+
 			                console.log(user.user + ' logged in')
 			                res.send(user.user+" login success")
 			            }
@@ -409,7 +412,7 @@ server.put('/applications/:id', updateApplicationById)
 server.put('/undeleteapplication/:id', undeleteApplicationById)
 
 
-//include routes 
+//include routes
 // let routes = require('./routes/index');
 // server.use('/', routes);
 

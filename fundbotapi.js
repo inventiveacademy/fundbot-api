32c076bf47c0b3f16be86ad752fca7f5cb5c55c0
rcrutherford <rcrutherford@gmail.com>
2017-09-27 18:07:36 -0500

    v0.9.2b with cors

--- fundbotapi.js
+++ fundbotapi.js
@@ -17,6 +17,13 @@ server.name = 'FundBot API'
 
 server.use(restify.plugins.bodyParser())
 server.use(restify.plugins.queryParser())
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
 
 
 // attach the session manager
